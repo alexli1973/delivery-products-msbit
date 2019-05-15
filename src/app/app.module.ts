6917022f65c02e1a37916ed9c63d6b1e3c6c919c
Alexander <alexl@netcloud.co.il>
2019-05-15 20:31:10 +0300

    added service for list items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListItemsComponent } from './list-items/list-items.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import {GetDataService} from './shared/services/get-data.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListItemsComponent,
+    ItemDetailsComponent,
+   // HttpClientModule,
+    // HttpClient
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [GetDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/list-items/list-items.component.ts
+++ src/app/list-items/list-items.component.ts
@@ -9,22 +9,39 @@ import {GetDataService} from '../shared/services/get-data.service';
 })
 export class ListItemsComponent implements OnInit {
 
-//  listItems: Item;
-  const listItems = {name};
-  private objNew: Item;
- // objNew;
+  listItems: Item[] = [];
+  apiData: any = [];
 
   constructor(private dataService: GetDataService) { }
 
   ngOnInit() {
-    this.dataService.getItems().subscribe((listItems: any) => this.listItems = listItems);
-   // const {creationDate, name} = this.listItems;
-    debugger;
-   // if (this.listItems) {
-   //  this.objNew = this.listItems;
-   //  console.log('NEW ', this.objNew);
-   // }
+    // this.dataService.getItems().subscribe((data: any) => this.apiData = data);
+    this.dataService.getItems().subscribe((data: any) => this.getItemsList(data));
+    // if (this.apiData) {
+    //   //debugger;
+    //   this.getItemsList(this.apiData);
+    //   console.log(this.apiData);
+    // }
+  }
 
+  getItemsList(arr) {
+    for (const i in arr) {
+      if (arr[i].hasOwnProperty('creationDate')) {
+        this.listItems.push(arr[i]);
+      } else {
+        Object.values(arr[i]).forEach(val => {
+          if (typeof val === 'object') {
+            if (!val.length) {
+              this.listItems.push(val);
+            } else {
+              val.forEach(val => {
+                this.listItems.push(val);
+              });
+            }
+          }
+        });
+      }
+    }
   }
 
 }
